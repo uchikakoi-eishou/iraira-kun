8a7b5d5b879d0698b57b3ef3cf4777f19f725495
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2019-05-02 23:49:45 +0900

    DB_ACCESS

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
-    $a = 1;
+    $mysqli = new mysqli("localhost", "iraira-kun", "aaamukatsuku", "iraira-kun");
+    if($mysqli->connect_errno){
+        printf("接続失敗: %s\n", $mysqli->connect_error);
+        exit();
+    }
 
+    $a = 1;
     if(isset($_POST['a'])){
         $a = $_POST['a'];
     }
     if(isset($_POST['plus'])){
         $a++;
+    }
+    if(isset($_POST['count_data'])){
+        $count_data = htmlspecialchars($_POST['count_data'], ENT_QUOTES);
+        if(!empty($count_data)){
+            $result = mysqli_query($mysqli, "insert into iraira_data(count_data) values('$count_data')");
+            header("localhost");
+        }
     }
  ?>
 
@@ -25,8 +37,26 @@
 </form>
 <p><?=$a?></p>
 
-<a href="" type="button">ログインして記録</a>
-<a href="" type="button">新規登録して記録</a>
+<form action="" method="POST">
+     <input type="hidden" name="count_data" value="<?=$a?>">
+     <input type="submit" name="" value="記録する">
+</form>
+
+<?php
+    $iraira_data = "select * from iraira_data order by id DESC";
+    $iraira_data = mysqli_query($mysqli, $iraira_data);
+    while($iraira_data_array = mysqli_fetch_assoc($iraira_data)){
+        echo $id = $iraira_data_array["id"];
+        echo ",";
+        echo $count_data = $iraira_data_array["count_data"];
+        echo ",";
+        echo $reg_date = $iraira_data_array["reg_date"];
+        echo "<br>";
+    }
+?>
+
+<!-- <a href="" type="button">ログインして記録</a> -->
+<!-- <a href="" type="button">新規登録して記録</a> -->
 
 </body>
 </html>
